cc45cb2bc6931361976dd418c66e3892fa60b32f
castetus <kboikov@mail.ru>
2021-11-01 12:03:21 +0300

    temp

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,74 @@
 <template>
-    <div class="map">
-        <h3>Карта офиса</h3>
+  <div class="map">
+    <h3>Карта офиса</h3>
 
-        <div
-            v-if="!isLoading"
-            class="map-root"
-        >
-            <!-- map -->
-        </div>
-        <div v-else>Loading...</div>
+    <div v-if="!isLoading" class="map-root">
+      <MapImg ref="svg" />
     </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
+import MapImg from "@/assets/images/map.svg";
+import * as d3 from "d3";
+
 export default {
-    data() {
-        return {
-            isLoading: false,
-        };
+  components: {
+    MapImg,
+  },
+  data() {
+    return {
+      isLoading: false,
+      svg: null,
+      g: null,
+    };
+  },
+  mounted() {
+    this.svg = d3.select(this.$refs.svg);
+    this.g = this.svg.select("g");
+    if (this.g) {
+      this.drawTables();
+    } else {
+      console.log("error");
+    }
+  },
+  methods: {
+    drawTables() {
+      const tablesGroup = this.g.append("g").classed("groupPlaces", true);
     },
+  },
 };
 </script>
 
 <style scoped>
 .map {
-    height: 100%;
-    width: 100%;
-    padding: 24px;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
 
 .map-root {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 ::v-deep svg {
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 }
 
 ::v-deep .table {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
